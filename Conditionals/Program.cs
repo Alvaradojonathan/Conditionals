3e2db066b83b31a80a5fbf5fde2604f4ec4902ee
Your Name <you@example.com>
2017-06-28 15:02:07 -0400

    conditionals practice

--- Conditionals/Program.cs
+++ Conditionals/Program.cs
@@ -10,6 +10,94 @@ namespace Conditionals
     {
         static void Main(string[] args)
         {
+            //Age distribution practice
+            //Console.WriteLine("Please type your age.");
+            //int age = int.Parse(Console.ReadLine());
+
+            //if (age >= 0 && age <= 2)
+            //{
+            //    Console.WriteLine("Still in mama's arms");
+            //}
+            //else if (age >= 3 && age <= 4)
+            //{
+            //    Console.WriteLine("Preschool Maniac");
+            //}
+            //else if (age >= 5 && age <= 11)
+            //{
+            //    Console.WriteLine("Elementary School");
+            //}
+            //else if (age >= 12 && age <= 14)
+            //{
+            //    Console.WriteLine("Middle School");
+            //}
+            //else if (age >= 15 && age <= 18)
+            //{
+            //    Console.WriteLine("High School");
+            //}
+            //else if (age >= 19 && age <= 22)
+            //{
+            //    Console.WriteLine("College");
+            //}
+            //else if (age >= 23 && age <= 65)
+            //{
+            //    Console.WriteLine("Working for the man");
+            //}
+            //else if (age >= 65 && age <= 100)
+            //{
+            //    Console.WriteLine("The golden years");
+            //}
+
+            ////fix this
+            //else 
+            //{
+            //    Console.WriteLine("Youre an alien, this program is for humans");
+            //}
+
+
+            //Word to digit
+
+            Console.WriteLine("Please type a number between 0 and 10 in lowercase word form");
+            string numberWord = Console.ReadLine();
+
+            switch (numberWord)
+            {
+                case "zero":
+                    Console.WriteLine("0");
+                    break;
+                case "one":
+                    Console.WriteLine("1");
+                    break;
+                case "two":
+                    Console.WriteLine("2");
+                    break;
+                case "three":
+                    Console.WriteLine("3");
+                    break;
+                case "four":
+                    Console.WriteLine("4");
+                    break;
+                case "five":
+                    Console.WriteLine("5");
+                    break;
+                case "six":
+                    Console.WriteLine("6");
+                    break;
+                case "seven":
+                    Console.WriteLine("7");
+                    break;
+                case "eight":
+                    Console.WriteLine("8");
+                    break;
+                case "nine":
+                    Console.WriteLine("9");
+                    break;
+                case "ten":
+                    Console.WriteLine(10);
+                    break;
+                default:
+                    Console.WriteLine("Error");
+                    break;
+            }
         }
     }
 }
