07a6e4424dbd36fb42f9352bcf7f4f510b3e7a53
Your Name <you@example.com>
2017-06-28 16:10:55 -0400

    gpa practice

--- Conditionals/Program.cs
+++ Conditionals/Program.cs
@@ -56,48 +56,77 @@ namespace Conditionals
 
             //Word to digit
 
-            Console.WriteLine("Please type a number between 0 and 10 in lowercase word form");
-            string numberWord = Console.ReadLine();
+            //Console.WriteLine("Please type a number between 0 and 10 in lowercase word form");
+            //string numberWord = Console.ReadLine();
 
-            switch (numberWord)
-            {
-                case "zero":
-                    Console.WriteLine("0");
-                    break;
-                case "one":
-                    Console.WriteLine("1");
-                    break;
-                case "two":
-                    Console.WriteLine("2");
-                    break;
-                case "three":
-                    Console.WriteLine("3");
-                    break;
-                case "four":
-                    Console.WriteLine("4");
-                    break;
-                case "five":
-                    Console.WriteLine("5");
-                    break;
-                case "six":
-                    Console.WriteLine("6");
-                    break;
-                case "seven":
-                    Console.WriteLine("7");
-                    break;
-                case "eight":
-                    Console.WriteLine("8");
-                    break;
-                case "nine":
-                    Console.WriteLine("9");
-                    break;
-                case "ten":
-                    Console.WriteLine(10);
-                    break;
-                default:
-                    Console.WriteLine("Error");
-                    break;
-            }
+            //switch (numberWord)
+            //{
+            //    case "zero":
+            //        Console.WriteLine("0");
+            //        break;
+            //    case "one":
+            //        Console.WriteLine("1");
+            //        break;
+            //    case "two":
+            //        Console.WriteLine("2");
+            //        break;
+            //    case "three":
+            //        Console.WriteLine("3");
+            //        break;
+            //    case "four":
+            //        Console.WriteLine("4");
+            //        break;
+            //    case "five":
+            //        Console.WriteLine("5");
+            //        break;
+            //    case "six":
+            //        Console.WriteLine("6");
+            //        break;
+            //    case "seven":
+            //        Console.WriteLine("7");
+            //        break;
+            //    case "eight":
+            //        Console.WriteLine("8");
+            //        break;
+            //    case "nine":
+            //        Console.WriteLine("9");
+            //        break;
+            //    case "ten":
+            //        Console.WriteLine(10);
+            //        break;
+            //    default:
+            //        Console.WriteLine("Error");
+            //        break;
+
+            //GPA Calc
+
+            double gradeA = 4.00d;
+            double gradeB = 3.00d;
+            double gradeC = 2.00d;
+            double gradeD = 1.00d;
+            double gradeF = 0.00d;
+
+            Console.WriteLine("How many classes did you take?");
+            double classesTaken = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("How many A's did you receive?");
+            double totalA = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("How many B's did you receive?");
+            double totalB = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("How many C's did you receive?");
+            double totalC = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("How many D's did you receive?");
+            double totalD = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("How many F's did you receive?");
+            double totalF = double.Parse(Console.ReadLine());
+
+            double gpa = Math.Round(((totalA * gradeA) + (totalB * gradeB) + (totalC * gradeC) + (totalD * gradeD) + (totalF * gradeF)) / classesTaken, 2);
+            Console.WriteLine("GPA is: " + gpa);
+
         }
     }
 }
